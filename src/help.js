40c435d5a3b9163d3d286ad4756057fbd78d6d32
Marius Schulz <marius.schulz@me.com>
2015-07-23 13:10:35 +0200

    Displays a pretty help screen

--- src/help.js
+++ src/help.js
@@ -1,5 +1,26 @@
+import chalk from "chalk";
+
 export default { show };
 
+const highlight = chalk.cyan;
+
+const ASCII_ART_HEADER =
+`   ____  _
+  / ___|| |_ _   ___  __
+  \\___ \\| __| | | \\ \\/ /
+   ___) | |_| |_| |>  <
+  |____/ \\__|\\__, /_/\\_\\
+             |___/
+`;
+
+const USAGE = `  styx ${highlight("<file>")} [--format ${highlight("<format>")}] [--help]`;
+
+const ARG_EXPLANATIONS = `
+    --format ${highlight("<format>")}    Specifies an output format: ${highlight("json")}, ${highlight("dot")}
+    --help               Displays this help text`;
+
 function show() {
-    console.log("<help>");
+    console.log(highlight(ASCII_ART_HEADER));
+    console.log(USAGE);
+    console.log(ARG_EXPLANATIONS);
 }
