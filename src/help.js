e2ffcd5e77ad30d80fe261089cef52b6a302c14a
Marius Schulz <marius.schulz@me.com>
2015-08-12 11:51:26 +0200

    Documents the `-g` option

--- src/help.js
+++ src/help.js
@@ -14,18 +14,21 @@ const ASCII_ART_HEADER =
              |___/
 `;
 
-const usage = `  styx [-o ${highlight("<file>")}] [-f ${highlight("<format>")}]
+const usage = `  styx [-o ${highlight("<file>")}] [-f ${highlight("<format>")}] [-g ${highlight("<graph id>")}]
        [--minified-json] [--help]`;
 
-const formattedOutputFormats = supportedOutputFormats.map(format => highlight(format)).join(" or ");
+const formattedOutputFormats = supportedOutputFormats
+    .map(format => highlight(format))
+    .join(" or ");
 
 const argExplanations = `
-    -o, --output ${highlight("<file>")}      Path to the output file
-    -f, --format ${highlight("<format>")}    The desired output format: ${formattedOutputFormats}
+    -o, --output ${highlight("<file>")}       Path to the output file
+    -f, --format ${highlight("<format>")}     Desired output format: ${formattedOutputFormats}
+    -g, --graph ${highlight("<graph id>")}    ID of the control flow graph to export
 
-    --minified-json          Minifies the serialized JSON string
+    --minified-json           Minifies the serialized JSON string
 
-    --help                   Displays this help text`;
+    --help                    Displays this help text`;
 
 function show() {
     console.log(highlight(ASCII_ART_HEADER));
